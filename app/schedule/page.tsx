fcc6996a7e56b86ae91ab358aac04819219fac41
AdebestAA <aadebesta@gmail.com>
2025-11-14 13:22:05 +0100

    play

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import { FaQuoteLeft } from 'react-icons/fa'
 
-// const text = ""
+
 export default function page() {
     return (
         <section className="">
